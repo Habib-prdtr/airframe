d82c09eb7da3afa3831e1c17e05129af314a0288
habib <habibin64@gmail.com>
2026-08-16 15:31:08 +0700

    fix: use relative paths for style and main script for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: true,
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'credentialless'
-    }
-  },
-  optimizeDeps: {
-    exclude: ['@mediapipe/tasks-vision']
-  }
+  base: './',
 });
